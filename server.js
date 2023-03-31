171e4d5f366c123444d47b32c01d39f24c9ca4b0
Mario Rodriguez Martinez <mariog.rodriguezm@gmail.com>
2023-03-31 11:18:48 +0200

    ID search fixed

--- server.js
+++ server.js
@@ -3,24 +3,18 @@ const phones = require('./data/phones.json');
 
 const app = express();
 
-// Route to show all phones
 app.get('/phones', (req, res) => {
   res.json(phones);
 });
 
-// Route to show phone details by ID
 app.get('/phones/:id', (req, res) => {
-  const id = req.params.id;
-  const phone = phones.find(phone => phone.id === id);
-
+  const phone = phones.find(p => p.id === Number(req.params.id));
   if (!phone) {
-    return res.status(404).json({ error: 'Phone not found' });
+    res.status(404).send('Phone not found');
+  } else {
+    res.json(phone);
   }
-
-  res.json(phone);
 });
 
-// Start server
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
-});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
